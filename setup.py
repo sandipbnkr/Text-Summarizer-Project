f5b10064ad6650e7e81ecf2b08cca5a872e201cb
Bankar Sandip (Dr.) <bankar.sandip@nmims.edu>
2024-07-20 12:03:48 +0530

    folder structure added latest

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md","r",encoding="utf-8") as f:
     long_description=f.read()
 
 
-__version__=="0.0.0"
+setuptools.__version__=="0.0.0"
 
 REPO_NAME="Text-Summarizer-Project"
 AUTHOR_USER_NAME="sandipbnkr"
